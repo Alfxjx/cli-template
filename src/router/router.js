95a730301ebf5fc07334047a9ee7ec0f804001bc
xujianxiang <xjxtju@163.com>
2021-01-26 16:19:52 +0800

    feat: router push overload

--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,12 @@ import { HomeRoutes } from "./modules/home.js";
 
 Vue.use(VueRouter);
 
+// ban掉router跳转自身时出现的报错
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err);
+};
+
 const routes = [...HomeRoutes];
 
 const router = new VueRouter({
