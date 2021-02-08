5ed94f48e3f85a195ab1dcdd71319235ad1ad595
xujianxiang <xjxtju@163.com>
2021-02-08 15:25:29 +0800

    feat: gzip and image comporession

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,32 @@
 const { VUE_APP_DEV_API } = process.env;
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
-const webpackConfig = {};
+const CompressionPlugin = require("compression-webpack-plugin");
+const webpackConfig = {
+  // 生产环境关闭sourceMap
+  productionSourceMap: false
+};
+
+const customOptions = {
+  mozjpeg: {
+    progressive: true,
+    quality: 50
+  },
+  optipng: {
+    enabled: true
+  },
+  pngquant: {
+    quality: [0.5, 0.65],
+    speed: 4
+  },
+  gifsicle: {
+    interlaced: false
+  },
+  // 不支持WEBP就不要写这一项
+  webp: {
+    quality: 75
+  }
+};
 
 if (process.env.NODE_ENV === "development") {
   webpackConfig.devServer = {
@@ -25,12 +50,28 @@ if (process.env.NODE_ENV === "development") {
 }
 
 if (process.env.NODE_ENV === "production") {
+  webpackConfig["chainWebpack"] = config => {
+    config.module
+      .rule("images")
+      .test(/\.(gif|png|jpe?g|svg)$/i)
+      .use("image-webpack-loader")
+      .loader("image-webpack-loader")
+      .options(customOptions)
+      .end();
+  };
   webpackConfig["configureWebpack"] = config => {
     config["plugins"].push(
       new BundleAnalyzerPlugin({
         analyzerMode: "static",
         openAnalyzer: false,
         reportFilename: `../reports/r-${new Date().getTime()}.html`
+      })
+    );
+    config["plugins"].push(
+      new CompressionPlugin({
+        test: /\.js$|\.html$|.\css/, // 匹配文件名
+        threshold: 10240, // 对超过10k的数据压缩
+        deleteOriginalAssets: true
       })
     );
   };
